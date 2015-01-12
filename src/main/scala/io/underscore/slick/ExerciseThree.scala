8f5de0d845c3fd837cc750548be88ae8c7a8cef0
Jonathan Ferguson <jono@spiralarm.com>
2015-01-12 16:12:29 +1100

    Align ExerciseThree with the current state of the book
     - Fixed drop / create code as we were always attempting to
       drop tables that didn't exist,
     - Use Timestamp instead of JodaTime - it will be reintroduced soon,
     - renamed fields from SQLite keywords when/from to ts/sender,
     - Worked around Auto increment issue in Slick or the SQLite driver
       where O.AutoInc was causing the field to be labeled as a primary key.
       This was accomplished by introducing a separate example ColourShape.

--- src/main/scala/io/underscore/slick/ExerciseThree.scala
+++ src/main/scala/io/underscore/slick/ExerciseThree.scala
@@ -2,56 +2,77 @@ package io.underscore.slick
 
 import scala.slick.driver.SQLiteDriver.simple._
 import scala.slick.jdbc.meta.MTable
-import org.joda.time.DateTime
 import scala.slick.model.PrimaryKey
+import java.sql.Timestamp
 
 object ExerciseThree extends Exercise {
 
+  final case class ColourShape(colour: Long, shape: String)
+
+  final class ColourShapeTable(tag: Tag) extends Table[ColourShape](tag, "colour_shape") {
+    def colour = column[Long]("colour")
+    def shape = column[String]("shape")
+
+    def pk = primaryKey("colour_shape_pk", (colour, shape))
+
+    def * = (colour, shape) <> (ColourShape.tupled, ColourShape.unapply)
+
+  }
+
+  lazy val coluredShapes = TableQuery[ColourShapeTable]
+
+  
   final case class User(id: Long, name: String)
 
   final class UserTable(tag: Tag) extends Table[User](tag, "user") {
-    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+    def id = column[Long]("id",O.AutoInc,O.PrimaryKey)
+    def ts = column[Timestamp]("ts")
     def name = column[String]("name")
+
     def * = (id, name) <> (User.tupled, User.unapply)
 
   }
 
   lazy val users = TableQuery[UserTable]
 
-  final case class Message(id: Long, from: Long, to: Option[Long], content: String, when: DateTime)
+  
+  final case class Message(id: Long, sender: Long, to: Option[Long], content: String, ts: Timestamp)
 
   final class MessageTable(tag: Tag) extends Table[Message](tag, "message") {
 
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
-    def fromId = column[Long]("from")
-    def from = foreignKey("from_fk", fromId, users)(_.id)
+    def senderId = column[Long]("sender")
+    def sender = foreignKey("sender_fk", senderId, users)(_.id)
     def toId = column[Option[Long]]("to")
     def to = foreignKey("to_fk", toId, users)(_.id)
     def content = column[String]("content")
-    def when = column[DateTime]("when")
+    def ts = column[Timestamp]("ts")
 
-    def * = (id, fromId, toId, content, when) <> (Message.tupled, Message.unapply)
+    def * = (id, senderId, toId, content, ts) <> (Message.tupled, Message.unapply)
   }
 
   lazy val messages = TableQuery[MessageTable]
 
   Database.forURL("jdbc:sqlite:essential-slick.db", user = "essential", password = "trustno1", driver = "org.sqlite.JDBC") withSession {
     implicit session ⇒
-      val ddl = messages.ddl ++ users.ddl
+      val tables = messages :: users :: coluredShapes :: Nil
 
-      //      Create Schema 
-      MTable.getTables(messages.baseTableRow.tableName).firstOption match {
-        case None ⇒
-          ddl.create
-        case Some(t) ⇒
-          ddl.drop
-          ddl.create
+      for {
+        table ← tables
+      } {
+        if (MTable.getTables(table.baseTableRow.tableName).firstOption.isDefined) {
+          println(s"drop ${table.baseTableRow.tableName}")
+          table.ddl.drop
+        }
+        table.ddl.create
       }
 
       val direct_message_query = for {
         message ← messages
         if message.toId.isDefined
       } yield message
+
+      println(direct_message_query)
 
   }
 
