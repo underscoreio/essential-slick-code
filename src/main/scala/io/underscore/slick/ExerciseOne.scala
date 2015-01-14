4673e25ea46b7d9bd0f1a5d5e2547c5592ac6bdf
Jonathan Ferguson <jono@spiralarm.com>
2015-01-14 19:12:15 +1100

    Update ExerciseOne to follow the Basics chapter.

--- src/main/scala/io/underscore/slick/ExerciseOne.scala
+++ src/main/scala/io/underscore/slick/ExerciseOne.scala
@@ -1,42 +1,48 @@
 package io.underscore.slick
 
 import java.sql.Timestamp
-import java.util.Properties
-
+import java.util.{ Date, Properties }
 import scala.slick.driver.SQLiteDriver.simple._
-
+import java.util.Calendar
 
 object ExerciseOne extends App {
 
-  final case class Message(id: Long,from: String, content: String, when: Timestamp)
+  final case class Message(id: Long, from: String, content: String, when: Timestamp)
 
   final class MessageTable(tag: Tag) extends Table[Message](tag, "message") {
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
-    def sender = column[String]("sender")  
+    def sender = column[String]("sender")
     def content = column[String]("content")
     def ts = column[Timestamp]("ts")
-    def * = (id,sender, content, ts) <> (Message.tupled, Message.unapply)
+    def * = (id, sender, content, ts) <> (Message.tupled, Message.unapply)
   }
 
   lazy val messages = TableQuery[MessageTable]
-
   
+  //Drive work around.
   val props = new Properties()
-  props.setProperty("date_precision","SECONDS")  
-  
-  Database.forURL("jdbc:sqlite:essential-slick.db", user = "essential", password = "trustno1", driver = "org.sqlite.JDBC", prop = props) withSession {
+  props.setProperty("date_precision", "SECONDS")
+
+  Database.forURL("jdbc:sqlite:essential-slick.db", driver = "org.sqlite.JDBC", prop = props) withSession {
     implicit session ⇒
 
       //Define a query 
-      val query = for {  
+      val halSays = for {
         message ← messages
         if message.sender === "HAL"
       } yield message
 
-      //Execute a query.
-      val messages_from_HAL: List[Message] = query.list
+      val aternateHalSays = messages.filter(_.sender === "HAL")
 
-      println(s" ${messages_from_HAL}")
+      //Date based query
+      val now = Calendar.getInstance()
+      now.add(Calendar.MINUTE, -30)
+      val recent: Timestamp = new Timestamp(now.getTimeInMillis())
+     
+      val recentMessages = halSays.filter(_.ts < recent)
+      
+      println(halSays.run)
+
   }
 
 }
