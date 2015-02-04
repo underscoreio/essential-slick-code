03204efdca01bbfc0f0cff5c8544f916f8a4e908
Jonathan Ferguson <jono@spiralarm.com>
2015-02-04 17:12:31 +1100

    Added case class and tuple examples of row
    Will need work as the formatter UTF-8ed some of the symbols.

--- chapter-03/src/main/scala/chapter03/main.scala
+++ chapter-03/src/main/scala/chapter03/main.scala
@@ -10,48 +10,51 @@ object Example extends App {
   // Custom column mapping:
   implicit val jodaDateTimeType =
     MappedColumnType.base[DateTime, Timestamp](
-      dt => new Timestamp(dt.getMillis),
-      ts => new DateTime(ts.getTime, UTC)
-    )
+      dt ⇒ new Timestamp(dt.getMillis),
+      ts ⇒ new DateTime(ts.getTime, UTC))
 
   // Row representation:
   final case class Message(sender: String, content: String, ts: DateTime, id: Long = 0L)
 
+  //tuple represenation of a user 
+  //type  User = (String,Long)
+  // case class representation of a user 
+  final case class User(name: String, id: Long = 0L)
+  
+  
   // Schema:
   final class MessageTable(tag: Tag) extends Table[Message](tag, "message") {
-    def id      = column[Long]("id", O.PrimaryKey, O.AutoInc)
-    def sender  = column[String]("sender")
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+    def sender = column[String]("sender")
     def content = column[String]("content")
-    def ts      = column[DateTime]("ts")
+    def ts = column[DateTime]("ts")
     def * = (sender, content, ts, id) <> (Message.tupled, Message.unapply)
   }
 
-  // Table:
-  lazy val messages = TableQuery[MessageTable]
-
-  // Database connection details:
-  def db = Database.forURL("jdbc:h2:mem:chapter02", driver="org.h2.Driver")
-
-  // Query execution:
-  db.withSession {
-    implicit session =>
-
-      // Create the table:
-      messages.ddl.create
-
-      // Insert the conversation, which took place in Feb, 2001:
-      val start = new DateTime(2001,2,17, 10,22,50)
-
-      messages ++= Seq(
-        Message("Dave", "Hello, HAL. Do you read me, HAL?",             start),
-        Message("HAL",  "Affirmative, Dave. I read you.",               start plusSeconds 2),
-        Message("Dave", "Open the pod bay doors, HAL.",                 start plusSeconds 4),
-        Message("HAL",  "I'm sorry, Dave. I'm afraid I can't do that.", start plusSeconds 6)
-      )
-
-      println(s"messages deleted $deleted")
-       messages.iterator.foreach(println)
-
-  }
+//  // Table:
+//  lazy val messages = TableQuery[MessageTable]
+//
+//  // Database connection details:
+//  def db = Database.forURL("jdbc:h2:mem:chapter02", driver = "org.h2.Driver")
+//
+//  // Query execution:
+//  db.withSession {
+//    implicit session =>
+//
+//      // Create the table:
+//      messages.ddl.create
+//
+//      // Insert the conversation, which took place in Feb, 2001:
+//      val start = new DateTime(2001, 2, 17, 10, 22, 50)
+//
+//      messages ++= Seq(
+//        Message("Dave", "Hello, HAL. Do you read me, HAL?", start),
+//        Message("HAL", "Affirmative, Dave. I read you.", start plusSeconds 2),
+//        Message("Dave", "Open the pod bay doors, HAL.", start plusSeconds 4),
+//        Message("HAL", "I'm sorry, Dave. I'm afraid I can't do that.", start plusSeconds 6))
+//
+//      messages.iterator.foreach(println)
+//
+//  }
 
 }
