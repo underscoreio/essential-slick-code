6aa6e9b99f58ddd00ed14fcd93c02fff8e96e8c1
Jonathan Ferguson <jono@spiralarm.com>
2015-04-21 14:15:23 +1000

    put applications into their own package using a single schema

--- chapter-04/src/main/scala/chapter04/joins/ExampleOfJoins.scala
+++ chapter-04/src/main/scala/chapter04/joins/ExampleOfJoins.scala
@@ -1,95 +1,10 @@
 package chapter04
+package joins
 
-import java.sql.Timestamp
+import org.joda.time._
+import chapter04.MessagingSchema._
 
-import scala.slick.lifted.MappedTo
-
-import org.joda.time.DateTime
-import org.joda.time.DateTimeZone._
-
-final case class Id[A](value:Long) extends AnyVal with MappedTo[Long]
-
-object ImplicitJoinsExample extends App {
-
-  trait Profile {
-    val profile: scala.slick.driver.JdbcProfile
-  }
-
-  trait Tables {
-    this: Profile =>
-
-    import profile.simple._
-
-    case class User(id: Option[Id[UserTable]], name: String, email: Option[String] = None)
-
-    class UserTable(tag: Tag) extends Table[User](tag, "user") {
-      def id    = column[Id[UserTable]]("id", O.AutoInc, O.PrimaryKey)
-      def name  = column[String]("name")
-      def email = column[Option[String]]("email")
-
-      def *     = (id.?, name, email) <> (User.tupled, User.unapply)
-    }
-
-    lazy val users  = TableQuery[UserTable]
-    lazy val insertUser = users returning users.map(_.id)
-
-    case class Room(title: String, id: Id[RoomTable] = Id[RoomTable](0L))
-
-    class RoomTable(tag: Tag) extends Table[Room](tag, "room") {
-      def id    = column[Id[RoomTable]]("id", O.PrimaryKey, O.AutoInc)
-      def title = column[String]("title")
-      def *     = (title, id) <> (Room.tupled, Room.unapply)
-    }
-
-    lazy val rooms = TableQuery[RoomTable]
-    lazy val insertRoom = rooms returning rooms.map(_.id)
-
-    case class Occupant(roomId: Id[RoomTable], userId: Id[UserTable])
-
-    class OccupantTable(tag: Tag) extends Table[Occupant](tag, "occupant") {
-      def roomId = column[Id[RoomTable]]("room")
-      def userId = column[Id[UserTable]]("user")
-      def pk     = primaryKey("occ_room_user_pk", (roomId, userId))
-      def user   = foreignKey("occ_user_fk", userId, users)(_.id)
-      def room   = foreignKey("occ_room_fk", roomId, rooms)(_.id)
-      def *      = (roomId, userId) <> (Occupant.tupled, Occupant.unapply)
-    }
-
-    lazy val occupants = TableQuery[OccupantTable]
-
-    implicit val jodaDateTimeType =
-      MappedColumnType.base[DateTime, Timestamp](
-        dt => new Timestamp(dt.getMillis),
-        ts => new DateTime(ts.getTime, UTC))
-
-    case class Message(senderId: Id[UserTable],
-                       content: String,
-                       ts: DateTime,
-                       id: Id[MessageTable] = Id(0L),
-                       toId: Option[Id[UserTable]] = None,
-                       roomId: Option[Id[RoomTable]] = None,
-                       readBy: Int)
-
-    class MessageTable(tag: Tag) extends Table[Message](tag, "message") {
-      def id       = column[Id[MessageTable]]("id", O.PrimaryKey, O.AutoInc)
-      def senderId = column[Id[UserTable]]("sender")
-      def content  = column[String]("content")
-      def toId     = column[Option[Id[UserTable]]]("to")
-      def roomId   = column[Option[Id[RoomTable]]]("room")
-      def ts       = column[DateTime]("ts")
-      def readBy   = column[Int]("readBy")
-      def *        = (senderId, content, ts, id, toId, roomId, readBy) <> (Message.tupled, Message.unapply)
-
-      def sender   = foreignKey("msg_sender_fk", senderId, users)(_.id)
-      def to       = foreignKey("msg_to_fk", toId, users)(_.id)
-      def room     = foreignKey("msg_room_fk", roomId, rooms)(_.id)
-    }
-
-    lazy val messages = TableQuery[MessageTable]
-
-  }
-
-  class Schema(val profile: scala.slick.driver.JdbcProfile) extends Tables with Profile
+object ExampleOfJoins extends App {
 
   val schema = new Schema(scala.slick.driver.H2Driver)
 
