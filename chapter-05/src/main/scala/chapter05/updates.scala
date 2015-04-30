d78fd0feecd0b3fbb6106ddcb65008bfaad3fdbe
Richard Dallaway <richard@dallaway.com>
2015-04-30 14:17:11 +0100

    Add example of plain sql updates

---
+++ chapter-05/src/main/scala/chapter05/updates.scala
@@ -0,0 +1,42 @@
+package chapter05
+
+import org.joda.time.DateTime
+import scala.slick.jdbc._
+import scala.slick.jdbc.{ StaticQuery => Q }
+import ChatSchema._
+import java.sql.Timestamp
+
+object PlainUpdatesExample extends App {
+
+  val schema = new Schema(scala.slick.driver.H2Driver)
+  import schema._, profile.simple._
+  def db = Database.forURL("jdbc:h2:mem:chapter04", driver="org.h2.Driver")
+
+  db.withSession { implicit session => 
+  
+    populate 
+  
+    import Q.interpolation
+
+    // Simple update example:
+    val char = "!"
+    val query = sqlu"""UPDATE "message" SET "content" = CONCAT("content", $char)"""
+    val excalimedRows = query.first
+    println(s"Exclamation mark added to $excalimedRows rows")
+    
+    // Using + and +?
+    val pattern = "%!"
+    val sensitive =  query + """ WHERE "content" NOT LIKE """ +? pattern
+    val sensitiveRows = sensitive.first
+    println(s"Exclamation mark added sensitively to $sensitiveRows rows")
+    
+    // Using DateTime as an example of a custom type
+    implicit object SetDateTime extends SetParameter[DateTime] {
+      def apply(dt: DateTime, pp: PositionedParameters): Unit =
+         pp >> new Timestamp(dt.getMillis)
+        // pp.setTimestamp(new Timestamp(dt.getMillis))  
+    }
+    val now = sqlu"""UPDATE message SET "ts" = """ +? DateTime.now
+   
+  }
+}
