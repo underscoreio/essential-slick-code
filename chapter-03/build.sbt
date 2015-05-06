53816877296411126f16d0f63af581f261e2a314
Richard Dallaway <richard@dallaway.com>
2015-05-06 10:27:07 +0100

    Aligned with book text.

--- chapter-03/build.sbt
+++ chapter-03/build.sbt
@@ -7,6 +7,4 @@ scalaVersion := "2.11.6"
 libraryDependencies ++= Seq(
   "com.typesafe.slick" %% "slick"           % "2.1.0",
   "com.h2database"      % "h2"              % "1.4.185",
-  "ch.qos.logback"      % "logback-classic" % "1.1.2",
-  "joda-time"           % "joda-time"       % "2.6",
-  "org.joda"            % "joda-convert"    % "1.2")
+  "ch.qos.logback"      % "logback-classic" % "1.1.2")
--- chapter-03/src/main/scala/chapter03/main.scala
+++ chapter-03/src/main/scala/chapter03/main.scala
@@ -1,36 +1,25 @@
 package chapter03
 
 import scala.slick.driver.H2Driver.simple._
-import java.sql.Timestamp
-import org.joda.time.DateTime
-import org.joda.time.DateTimeZone.UTC
 
 object Example extends App {
 
-  // Custom column mapping:
-  implicit val jodaDateTimeType =
-    MappedColumnType.base[DateTime, Timestamp](
-      dt => new Timestamp(dt.getMillis),
-      ts => new DateTime(ts.getTime, UTC)
-    )
-
   // Row representation:
-  final case class Message(sender: String, content: String, ts: DateTime, id: Long = 0L)
+  final case class Message(sender: String, content: String, id: Long = 0L)
 
   // Schema:
   final class MessageTable(tag: Tag) extends Table[Message](tag, "message") {
     def id      = column[Long]("id", O.PrimaryKey, O.AutoInc)
     def sender  = column[String]("sender")
     def content = column[String]("content")
-    def ts      = column[DateTime]("ts")
-    def * = (sender, content, ts, id) <> (Message.tupled, Message.unapply)
+    def * = (sender, content, id) <> (Message.tupled, Message.unapply)
   }
 
   // Table:
   lazy val messages = TableQuery[MessageTable]
 
   // Database connection details:
-  def db = Database.forURL("jdbc:h2:mem:chapter02", driver="org.h2.Driver")
+  def db = Database.forURL("jdbc:h2:mem:chapter03", driver="org.h2.Driver")
 
   // Query execution:
   db.withSession {
@@ -39,45 +28,36 @@ object Example extends App {
       // Create the table:
       messages.ddl.create
 
-      // Insert the conversation, which took place in Feb, 2001:
-      val start = new DateTime(2001,2,17, 10,22,50)
-
+      // Add some data:
       messages ++= Seq(
-        Message("Dave", "Hello, HAL. Do you read me, HAL?",             start),
-        Message("HAL",  "Affirmative, Dave. I read you.",               start plusSeconds 2),
-        Message("Dave", "Open the pod bay doors, HAL.",                 start plusSeconds 4),
-        Message("HAL",  "I'm sorry, Dave. I'm afraid I can't do that.", start plusSeconds 6)
+        Message("Dave", "Hello, HAL. Do you read me, HAL?"),
+        Message("HAL",  "Affirmative, Dave. I read you."),
+        Message("Dave", "Open the pod bay doors, HAL."),
+        Message("HAL",  "I'm sorry, Dave. I'm afraid I can't do that.")
       )
-
-      // Delete all messages from HAL:
-      val rowsDeleted = messages.filter(_.sender === "HAL").delete
-      println(s"Result of deleting HAL messages: $rowsDeleted")
 
       // Insert one, returning the ID:
       val id =
-        (messages returning messages.map(_.id)) += Message("HAL", "I'm back", DateTime.now)
+        (messages returning messages.map(_.id)) += Message("HAL", "I'm back")
       println(s"The ID inserted was: $id")
 
-      // Update HAL's name and the time of the message:
-      val query = for {
-        message <- messages
-        if message.sender === "HAL"
-      } yield (message.sender, message.ts)
+      // Update HAL's name:
+      val rows = messages.filter(_.sender === "HAL").map(_.sender).update("HAL 9000")
 
-      val rowsAffected = query.update("HAL 9000", DateTime.now)
+      // Update HAL's name and message:
+      val query =
+        messages.
+        filter(_.id === 4L).
+        map(message => (message.sender, message.content))
+
+      val rowsAffected = query.update("HAL 9000", "Sure, Dave. Come right in.")
+
+      // Delete messages from HAL:
+      // NB: will be zero rows affeced becase we've renamed HAL to HALL 9000
+      messages.filter(_.sender === "HAL").delete
 
       // Current state of the database:
+      println("\nState of the database:")
       messages.iterator.foreach(println)
-
-      // Plain SQL Query example:
-      import scala.slick.jdbc.StaticQuery.interpolation
-
-      val plainQuery =
-        sqlu"""UPDATE "message" SET "content" = CONCAT("content", '!')"""
-
-      val numRowsModified = plainQuery.first
-
-      println(s"Rows changed via plain query: $numRowsModified")
   }
-
 }
