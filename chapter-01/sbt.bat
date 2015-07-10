1876cfda58928a65aa2e5b0893318424e22c4b8b
Richard Dallaway <richard@dallaway.com>
2015-07-10 14:45:10 +0100

    Reduce memory requirements.
    Friendlier to 32bit JVMs

--- chapter-01/sbt.bat
+++ chapter-01/sbt.bat
@@ -1,1 +1,1 @@
-java -Xmx3g -Xms3g -XX:+TieredCompilation -XX:ReservedCodeCacheSize=256m -XX:MaxPermSize=512m -XX:+UseNUMA -XX:+UseParallelGC -XX:+CMSClassUnloadingEnabled -jar ../lib/sbt-launch.jar %*
+java -Xmx1g -XX:+TieredCompilation -XX:ReservedCodeCacheSize=256m -XX:MaxPermSize=512m -XX:+UseNUMA -XX:+UseParallelGC -XX:+CMSClassUnloadingEnabled -jar ../lib/sbt-launch.jar %*
