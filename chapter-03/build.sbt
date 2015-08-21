df392ee921d3c338fcc44ddf2efc4f75bb2ecacc
Jonathan Ferguson <jono@spiralarm.com>
2015-08-21 13:55:14 +1000

    Chapter 3 updating to Slick 3
    - added initial console commands
    - updated queries to use DBIO Actions

--- chapter-03/build.sbt
+++ chapter-03/build.sbt
@@ -20,3 +20,9 @@ libraryDependencies ++= Seq(
   "com.typesafe.slick" %% "slick"           % "3.0.0",
   "com.h2database"      % "h2"              % "1.4.185",
   "ch.qos.logback"      % "logback-classic" % "1.1.2")
+
+initialCommands in console := """
+  |import slick.driver.H2Driver.api._
+  |import Example._
+  |Example.main(Array())
+""".trim.stripMargin
--- chapter-03/src/main/scala/main.scala
+++ chapter-03/src/main/scala/main.scala
@@ -1,11 +1,9 @@
-package chapter03
-
-
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
-
 import slick.driver.H2Driver.api._
+import slick.lifted.ProvenShape.proveShapeOf
+
 
 object Example extends App {
 
@@ -61,14 +59,69 @@ object Example extends App {
 
     val rowsAffected  = exec(query.update(("HAL 9000", "Sure, Dave. Come right in.")))
 
-
+    messages.
+      filter(_.id === 4L).
+      map(message => (message.sender, message.content)).
+      updateStatement
+    
+    exec(
+      messages.
+        filter(_.id === 4L).
+        map(message => (message.sender, message.content)).
+        update(("HAL 9000", "Sure, Dave. Come right in.")))    
+    
+    exec(messages returning messages.map(_.id) += Message("Dave", "Point taken." ))
     // Delete messages from HAL:
     // NB: will be zero rows affected because we've renamed HAL to HALL 9000
     exec(messages.filter(_.sender === "HAL").delete)
 
-    // Current state of the database:
-    println("\nState of the database:")
-    exec(messages.result.map(_.foreach(println)))
+    def exclaim(msg: Message): Message = msg.copy(content = msg.content + "!")    
+    
+    for {
+      msg <- exec(messages.result)
+    } yield exec(messages.filter(_.id === msg.id).update(exclaim(msg)))
+    
+    def updateContent(id: Long) =
+      messages.filter(_.id === id).map(_.content)
+
+    def currentState() = {
+         println("\nState of the database:")
+         exec(messages.result.map(_.foreach(println)))
+    }
+    
+    currentState
+     
+    try {
+      exec {
+        (
+        updateContent(2L).update("Blue Mooon")                          andThen
+        updateContent(3L).update("Please, anything but your singing ")  andThen
+        messages.result.map(_.foreach { println })                      andThen
+        DBIO.failed(new Exception("agggh my ears"))                     andThen
+        updateContent(4L).update("That's incredibly hurtful")
+        ).transactionally
+      }
+    
+    } catch {
+      case weKnow: Throwable => println("expected")
+    }
+        
+    currentState
   } finally db.close
 
+  //Exercises
+
+  //Insert only once 
+  def insertOnce(sender: String, text: String): Long = ???
+  
+  println(insertOnce("Dave","Have you changed the locks?") == insertOnce("Dave","Have you changed the locks?"))
+  
+  //Update Using a For Comprehension
+  val rowsAffected = messages.
+                     filter(_.sender === "HAL").
+                     map(msg => (msg.sender)).
+                     update("HAL 9000")  
+  
+  val rowsAffectedUsingForComprehension = ??? 
+  
 }
