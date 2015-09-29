625fc8519c48d9999edac51e78362d628136f986
Richard Dallaway <richard@dallaway.com>
2015-09-29 20:39:09 +0100

    commented and flagged as exercise solution

--- chapter-04/src/main/scala/nested_case_class.scala
+++ chapter-04/src/main/scala/nested_case_class.scala
@@ -2,7 +2,8 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import slick.driver.JdbcProfile
-import slick.lifted.ProvenShape.proveShapeOf
+
+// Solution to exercixe 4.2.4.1
 
 object NestedCaseClassExampleApp extends App {
 
@@ -15,52 +16,56 @@ object NestedCaseClassExampleApp extends App {
 
     import profile.api._
 
-  case class EmailContact(name: String, email: String)
-  case class Address(street: String, city: String, country: String)
+    // We want to represent a row in this structure:
+    case class EmailContact(name: String, email: String)
+    case class Address(street: String, city: String, country: String)
+    case class User(contact: EmailContact, address: Address, id: Long = 0L)
 
-  case class User(contact: EmailContact, address: Address, id: Long = 0L)
+    // ...but our legacy table looks like this:
+    final class UserTable(tag: Tag) extends Table[User](tag, "user") {
+      def id           = column[Long]("id", O.PrimaryKey, O.AutoInc)
+      def name         = column[String]("name")
+      def age          = column[Int]("age")
+      def gender       = column[Char]("gender")
+      def height       = column[Float]("height_m")
+      def weight       = column[Float]("weight_kg")
+      def shoeSize     = column[Int]("shoe_size")
+      def email        = column[String]("email_address")
+      def phone        = column[String]("phone_number")
+      def accepted     = column[Boolean]("terms")
+      def sendNews     = column[Boolean]("newsletter")
+      def street       = column[String]("street")
+      def city         = column[String]("city")
+      def country      = column[String]("country")
+      def faveColor    = column[String]("fave_color")
+      def faveFood     = column[String]("fave_food")
+      def faveDrink    = column[String]("fave_drink")
+      def faveTvShow   = column[String]("fave_show")
+      def faveMovie    = column[String]("fave_movie")
+      def faveSong     = column[String]("fave_song")
+      def lastPurchase = column[String]("sku")
+      def lastRating   = column[Int]("service_rating")
+      def tellFriends  = column[Boolean]("recommend")
+      def petName      = column[String]("pet")
+      def partnerName  = column[String]("partner")
 
-  final class UserTable(tag: Tag) extends Table[User](tag, "user") {
-    def id           = column[Long]("id", O.PrimaryKey, O.AutoInc)
-    def name         = column[String]("name")
-    def age          = column[Int]("age")
-    def gender       = column[Char]("gender")
-    def height       = column[Float]("height_m")
-    def weight       = column[Float]("weight_kg")
-    def shoeSize     = column[Int]("shoe_size")
-    def email        = column[String]("email_address")
-    def phone        = column[String]("phone_number")
-    def accepted     = column[Boolean]("terms")
-    def sendNews     = column[Boolean]("newsletter")
-    def street       = column[String]("street")
-    def city         = column[String]("city")
-    def country      = column[String]("country")
-    def faveColor    = column[String]("fave_color")
-    def faveFood     = column[String]("fave_food")
-    def faveDrink    = column[String]("fave_drink")
-    def faveTvShow   = column[String]("fave_show")
-    def faveMovie    = column[String]("fave_movie")
-    def faveSong     = column[String]("fave_song")
-    def lastPurchase = column[String]("sku")
-    def lastRating   = column[Int]("service_rating")
-    def tellFriends  = column[Boolean]("recommend")
-    def petName      = column[String]("pet")
-    def partnerName  = column[String]("partner")
+      // Packing a row into the structure we want:
+      def pack(row: (String, String, String, String, String, Long)): User =
+        User(
+          EmailContact(row._1, row._2),
+          Address(row._3, row._4, row._5), row._6)
 
-    def pack(row: (String, String, String, String, String, Long)): User =
-      User(
-        EmailContact(row._1, row._2),
-        Address(row._3, row._4, row._5), row._6)
+      // Unacking our structure into a row representation:
+      def unpack(user: User): Option[(String, String, String, String, String, Long)] =
+        Some((user.contact.name, user.contact.email, user.address.street, user.address.city, user.address.country, user.id))
 
-    def unpack(user: User): Option[(String, String, String, String, String, Long)] =
-      Some((user.contact.name, user.contact.email, user.address.street, user.address.city, user.address.country, user.id))
+      // We only project the columns we are interested in:
+      def * = (name, email, street, city, country, id) <> (pack, unpack)
+    }
 
-     def * = (name, email, street, city, country, id) <> (pack, unpack)
+    lazy val users = TableQuery[UserTable]
   }
 
-  lazy val users = TableQuery[UserTable]
-
-  }
   class Schema(val profile: JdbcProfile) extends Tables with Profile
 
   val schema = new Schema(slick.driver.H2Driver)
@@ -70,17 +75,22 @@ object NestedCaseClassExampleApp extends App {
   def exec[T](action: DBIO[T]): T =
     Await.result(db.run(action), 2 seconds)
 
-  // Database connection details:
   val db = Database.forConfig("chapter04")
 
-  val program = for {
+  // Insert one user:
+  val setup = for {
     _ <- users.schema.create
     i <- users += User(
-      EmailContact("Dr. Dave Bowman", "dave@example.org"),
-      Address("123 Some Street", "Any Town", "USA"))
+          EmailContact("Dr. Dave Bowman", "dave@example.org"),
+          Address("123 Some Street", "Any Town", "USA")
+        )
     xs <- users.result
   } yield xs
 
-  exec(program).foreach { println }
+
+  exec(setup)
+
+  println("\nContents of the user table:")
+  println(exec(users.result))
 
 }
