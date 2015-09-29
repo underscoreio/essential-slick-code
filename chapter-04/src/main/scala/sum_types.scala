342437d967111868503d9a4182dc63520cdd373d
Richard Dallaway <richard@dallaway.com>
2015-09-29 20:30:58 +0100

    Comments and printlns

--- chapter-04/src/main/scala/sum_types.scala
+++ chapter-04/src/main/scala/sum_types.scala
@@ -1,12 +1,10 @@
-import java.sql.Timestamp
-import org.joda.time.DateTime
-import org.joda.time.DateTimeZone.UTC
 import slick.driver.JdbcProfile
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import slick.lifted.MappedTo
-import slick.lifted.ProvenShape.proveShapeOf
+
+// Code relating to 4.4.2 "Sum Types"
 
 object SumTypesExample extends App {
 
@@ -14,6 +12,8 @@ object SumTypesExample extends App {
     val profile: JdbcProfile
   }
 
+  // We represent primary keys using these mapped value classes
+  // described in section 4.4.1
   object PKs {
     import slick.lifted.MappedTo
     case class MessagePK(value: Long) extends AnyVal with MappedTo[Long]
@@ -25,11 +25,6 @@ object SumTypesExample extends App {
 
     import profile.api._
     import PKs._
-
-    implicit val jodaDateTimeType =
-      MappedColumnType.base[DateTime, Timestamp](
-        dt => new Timestamp(dt.getMillis),
-        ts => new DateTime(ts.getTime, UTC))
 
     case class User(name: String, id: UserPK = UserPK(0L))
 
@@ -43,13 +38,17 @@ object SumTypesExample extends App {
     lazy val users = TableQuery[UserTable]
     lazy val insertUser = users returning users.map(_.id)
 
-
+    //
+    // The sum type we want to represent:
+    //
     sealed trait Flag
     case object Important extends Flag
     case object Offensive extends Flag
     case object Spam extends Flag
-    // case object OffTopic extends Flag
 
+    //
+    // The custom mapping into our arbitrary database representation:
+    //
     implicit val flagType =
       MappedColumnType.base[Flag, Char](
         f => f match {
@@ -66,7 +65,6 @@ object SumTypesExample extends App {
     case class Message(
         senderId: UserPK,
         content:  String,
-        ts:       DateTime,
         flag:     Option[Flag] = None,
         id:       MessagePK = MessagePK(0L))
 
@@ -75,9 +73,8 @@ object SumTypesExample extends App {
       def senderId = column[UserPK]("sender")
       def content  = column[String]("content")
       def flag     = column[Option[Flag]]("flag")
-      def ts       = column[DateTime]("ts")
 
-      def * = (senderId, content, ts, flag, id) <> (Message.tupled, Message.unapply)
+      def * = (senderId, content, flag, id) <> (Message.tupled, Message.unapply)
 
       def sender = foreignKey("sender_fk", senderId, users)(_.id, onDelete=ForeignKeyAction.Cascade)
     }
@@ -97,24 +94,22 @@ object SumTypesExample extends App {
 
   val db = Database.forConfig("chapter04")
 
-  // Insert the conversation, which took place in Feb, 2001:
-  val start = new DateTime(2001, 2, 17, 10, 22, 50)
-
   val program =
     for {
     _      <- ddl.create
     halId  <- insertUser += User("HAL")
     daveId <- insertUser += User("Dave")
     count  <- messages ++= Seq(
-               Message(daveId, "Hello, HAL. Do you read me, HAL?", start),
-               Message(halId,  "Affirmative, Dave. I read you.", start plusSeconds 2),
-               Message(daveId, "Open the pod bay doors, HAL.", start plusSeconds 4),
-               Message(halId,  "I'm sorry, Dave. I'm afraid I can't do that.", start plusSeconds 6, Some(Important)))
+               Message(daveId, "Hello, HAL. Do you read me, HAL?"),
+               Message(halId,  "Affirmative, Dave. I read you."),
+               Message(daveId, "Open the pod bay doors, HAL."),
+               Message(halId,  "I'm sorry, Dave. I'm afraid I can't do that.", Some(Important)))
     msgs   <- messages.filter(_.flag === (Important : Flag)).result
   } yield  msgs
 
   val result =  Await.result(db.run(program), 2 seconds)
 
+  println("\nThe list of important messages:")
   println(result)
 
 }
